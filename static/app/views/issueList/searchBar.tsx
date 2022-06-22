aebd8a768575672a3559080fa563a2bdce704dfb
jennmueng <ldrwarlock@gmail.com>
2022-06-22 12:24:02 -0700

    ref: Clear unused issues searchbar code

--- static/app/views/issueList/searchBar.tsx
+++ static/app/views/issueList/searchBar.tsx
@@ -1,6 +1,5 @@
 import {Component} from 'react';
 
-import {fetchRecentSearches} from 'sentry/actionCreators/savedSearches';
 import {Client} from 'sentry/api';
 import SmartSearchBar from 'sentry/components/smartSearchBar';
 import {
@@ -83,28 +82,8 @@ class IssueListSearchBar extends Component<Props, State> {
   componentDidMount() {
     // Ideally, we would fetch on demand (e.g. when input gets focus)
     // but `<SmartSearchBar>` is a bit complicated and this is the easiest route
-    this.fetchData();
     this.getSupportedTags();
   }
-
-  fetchData = async () => {
-    this.props.api.clear();
-    const resp = await this.getRecentSearches();
-
-    this.setState({
-      defaultSearchItems: [
-        SEARCH_ITEMS,
-        resp
-          ? resp.map(query => ({
-              desc: query,
-              value: query,
-              type: ItemType.RECENT_SEARCH,
-            }))
-          : [],
-      ],
-      recentSearches: resp,
-    });
-  };
 
   getSupportedTags = () => {
     const {supportedTags} = this.props;
@@ -134,21 +113,6 @@ class IssueListSearchBar extends Component<Props, State> {
     return values.map(({value}) => value);
   };
 
-  getRecentSearches = async (): Promise<string[]> => {
-    const {api, organization} = this.props;
-    const recent = await fetchRecentSearches(
-      api,
-      organization.slug,
-      SavedSearchType.ISSUE
-    );
-    return recent?.map(({query}) => query) ?? [];
-  };
-
-  handleSavedRecentSearch = () => {
-    // Reset recent searches
-    this.fetchData();
-  };
-
   render() {
     const {tagValueLoader: _, savedSearch, sort, onSidebarToggle, ...props} = this.props;
 
@@ -160,8 +124,6 @@ class IssueListSearchBar extends Component<Props, State> {
         hasRecentSearches
         savedSearchType={SavedSearchType.ISSUE}
         onGetTagValues={this.getTagValues}
-        // defaultSearchItems={this.state.defaultSearchItems}
-        onSavedRecentSearch={this.handleSavedRecentSearch}
         actionBarItems={[
           makePinSearchAction({sort, pinnedSearch}),
           makeSaveSearchAction({sort}),
